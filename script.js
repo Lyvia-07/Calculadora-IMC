f2e387980a44aecb20c72c557cde1ff191954a70
Lyvia-07 <lyvia.m.lima@aluno.senai.br>
2025-06-17 17:28:56 -0300

    calculadora

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-const imcForm= document.getElementById("imcForm");
+const imcForm = document.getElementById("imcForm");
 const peso = document.getElementById("peso");
-const altura= document.getElementById("altura");
-const resultado = document.getElementById("resultado")
+const altura = document.getElementById("altura");
+const resultado = document.getElementById("resultado");
 
-imcForm.addEventListener('submit',function (event){
-    event.preventDefault();
-    calcularIMC(peso.value, altura.value);
+imcForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  calcularIMC(peso.value, altura.value);
+});
+function calcularIMC(p, a) {
+  const conta = p / (a * a);
 
-})
-function calcularIMC(p, a){
-    const conta = p / (a*a);
+  if (conta < 18.5) {
+    classificacao = "baixo do peso";
+  } else if (conta <= 24.9) {
+    classificacao = "peso normal";
+  } else if (conta <= 29.9) {
+    classificacao = "sobrepeso";
+  } else if (conta <= 34.9) {
+    classificacao = "obesidade grau I";
+  } else if (conta <= 39.9) {
+    classificacao = "obesidade grau II";
+  } else {
+    classificacao = "obesidade grau III";
+  }
+  console.log(`Seu peso é ${p}, sua altura é ${a} e seu IMC é ${conta.toFixed(2)}, seu nível é ${classificacao}`)
 
-if (conta < 18.5){
-    classificacao = "baixo do peso"}
-
-else if (conta <= 24.9){
-    classificacao = "peso normal"}
-
-else if (conta <= 29.9 ){
-    classificacao = "sobrepeso"}
-
-else if (conta <= 34.9){
-      classificacao = "obesidade grau I"}
-
-else if (conta <= 39.9){
-        classificacao = "obesidade grau II"}
-
-else{ classificacao = "obesidade grau III"
-}
-
-resultado.textContent = `Seu peso é ${p}, sua altura é ${a} e seu IMC é ${conta}, seu nível é ${classificacao}`
-
+  resultado.textContent = `Seu peso é ${p}, sua altura é ${a} e seu IMC é ${conta.toFixed(2)}, seu nível é ${classificacao}`;
 }
